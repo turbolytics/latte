d7027b18815dcd6be460977cce48b19119f79339
Daniel Mican <danielmican@Daniels-MacBook-Pro.local>
2023-12-18 19:04:41 -0500

    feat(Mongo): Mongo source
    refs #14

---
+++ dev/mongo/init-db.js
@@ -0,0 +1,22 @@
+db.users.insertMany([
+    {
+        account: 'amazon',
+        signup_time: new ISODate(),
+    },
+    {
+        account: 'amazon',
+        signup_time: new ISODate(),
+    },
+    {
+        account: 'google',
+        signup_time: new ISODate(),
+    },
+    {
+        account: 'google',
+        signup_time: new ISODate(),
+    },
+    {
+        account: 'google',
+        signup_time: new ISODate(),
+    }
+]);
