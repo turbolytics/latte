0d7ed405d1e0d9e25fdea68b3a388e7b2eafbcbb
Daniel Mican <danielmican@Daniels-MacBook-Pro.local>
2024-01-02 06:04:51 -0500

    Generic maps to metrics

--- internal/sources/postgres/postgres.go
+++ internal/sources/postgres/postgres.go
@@ -3,12 +3,10 @@ package postgres
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	_ "github.com/lib/pq"
 	"github.com/mitchellh/mapstructure"
 	"github.com/turbolytics/collector/internal/metrics"
 	scsql "github.com/turbolytics/collector/internal/sources/sql"
-	"strconv"
 )
 
 type config struct {
@@ -19,36 +17,6 @@ type config struct {
 type Postgres struct {
 	config config
 	db     *sql.DB
-}
-
-func resultsToMetrics(results []map[string]any) ([]*metrics.Metric, error) {
-	var ms []*metrics.Metric
-	for _, r := range results {
-		val, ok := r["value"]
-		if !ok {
-			return nil, fmt.Errorf("each row must contain a %q key", "value")
-		}
-
-		m := metrics.New()
-
-		switch v := val.(type) {
-		case int:
-			m.Value = float64(v)
-		case string:
-			tv, err := strconv.ParseFloat(v, 64)
-			if err != nil {
-				return nil, fmt.Errorf("unable to parse string to float: %q", v)
-			}
-			m.Value = tv
-		}
-		delete(r, "value")
-		for k, v := range r {
-			m.Tags[k] = v.(string)
-		}
-		ms = append(ms, &m)
-	}
-
-	return ms, nil
 }
 
 func (p *Postgres) Source(ctx context.Context) ([]*metrics.Metric, error) {
@@ -63,7 +31,7 @@ func (p *Postgres) Source(ctx context.Context) ([]*metrics.Metric, error) {
 		return nil, err
 	}
 
-	ms, err := resultsToMetrics(results)
+	ms, err := metrics.MapsToMetrics(results)
 	return ms, err
 }
 
